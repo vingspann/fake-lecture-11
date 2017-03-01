a0862f55d92e00059aa8a94f3f7e7ed23ce576b4
Sean Carpenter <iblvinathgcldlv@gmail.com>
2017-03-01 03:21:23 +0000

    added circle.yml

--- functions.py
+++ functions.py
@@ -2,15 +2,18 @@ def get_chatbot_response(message):
     if message[:2] != '!!':
         return ''
 
-    points, command, args = message.split(' ', 2)
+    points, command, args = message.split(" ", 2)
     if command == 'hello':
         return 'Hey there!'
     elif command == 'add':
-        num1, num2 = args.split()
-        return num1 + num2
+        num1, num2 = args.split(" ")
+        return int(num1) + int(num2)
     elif command == 'divide':
-        num1, num2 = args.split()
-        return num1 / num2
+        num1, num2 = args.split(" ")
+        if num2 == 0:
+            return 'Cannot divide by zero'
+        else:
+            return int(num1) / int(num2)
     elif command == 'say':
         return args
     else:
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,6 +5,17 @@ class ChatBotResponseTest(unittest.TestCase):
     def test_not_command(self):
         response = functions.get_chatbot_response('Potato')
         self.assertEquals(response, '')
-
+    def test_hello_command(self):
+        response = functions.get_chatbot_response('!! hello there')
+        self.assertEquals(response, 'Hey there!')
+    def test_add_command(self):
+        response = functions.get_chatbot_response('!! add 2 3')
+        self.assertEquals(response, 5)
+    def test_divide_command(self):
+        response = functions.get_chatbot_response('!! divide 6 2')
+        self.assertEquals(response, 3)
+    def test_say_command(self):
+        response = functions.get_chatbot_response('!! say butts')
+        self.assertEquals(response, 'butts')
 if __name__ == '__main__':
     unittest.main()
